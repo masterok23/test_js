a7ffc05e764986871edcb54e2a0dbadbbb2f3eae
masterok23 <master2306@mail.ru>
2020-06-19 22:18:26 +0300

    lesson 8

--- root/script.js
+++ root/script.js
@@ -1,69 +1,30 @@
-'use strict';
-let a = 5;
-console.log(a);
+let num = 50;
+
+if (num < 49) {
+    console.log('Неверно!')
+} else if (num > 100) {
+    console.log('Много!')
+} else {
+    console.log('Верно!')
+};
 
 
+(num == 50) ? console.log('Верно!') : console.log('Неверно!'); 
 
-// // const obj = {
-// //         name: "Alex"
-// // }
- 
-// // const arr = [1,2,3,4,5];
-
-// // console.log(obj.name + arr[1]);
-
-// // - to String
-
-// // 1)
-// console.log(typeof(String(1)));
-
-// // 2)
-// console.log('' + 2);
-
-// console.log('https://vk.com/catalog/'+5);
-
-// console.log(typeof(5 + +'5'));
-
-// console.log(parseInt('15px',10));
-
-// //let ans = + prompt("Hello?",'');
-// //console.log('ans'/ '2');
-
-// let switcher = null;
-
-// if (switcher) {
-//     console.log("Working..")
-// }
-
-// switcher = 1;
-
-// if (switcher) {
-//     console.log("Working..")
-// }
-
-// console.log(typeof(!!'4'));
-
-//alert('q');
-// let answer = confirm("Как дела?");
-
-// console.log(answer); 
-// let arr = ['plum.png','orange.jpg','appme.bmp'];
-
-// //let answer = prompt("есть ли вам 18", "Да");
-
-// //console.log(typeof(arr[1])); 
-
-// let incr = 10,
-//     decr = 10;
-
-// //incr++;
-// //decr--;
-// console.log(++incr, --decr);
-
-// console.log(5%3);
-// console.log(2 == '2');
-
-// let isChecked = true,
-//     isClosed = true;
-
-// console.log(isChecked || isClosed);
+switch(num == 50){
+    case num < 49:
+        console.log('Неверно!');
+        break;
+    case num > 100:
+        console.log('Много!');
+        break;
+    case num > 80:
+        console.log('Все еще много!');
+        break;
+    case num = 50:
+        console.log('Верно!');
+        break;
+    default:
+        console.log('Что-то пошло не так...');
+        break;
+}
